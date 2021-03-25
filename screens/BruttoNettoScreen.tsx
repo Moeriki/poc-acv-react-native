4ea91d87a2849426e056f8f137ed5e1d4e710049
Dieter Luypaert <dieterluypaert@gmail.com>
2021-03-25 11:57:03 +0100

    ✨ manage web view URL changes

--- screens/BruttoNettoScreen.tsx
+++ screens/BruttoNettoScreen.tsx
@@ -1,13 +1,31 @@
+import * as Linking from 'expo-linking';
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { WebViewNativeEvent } from 'react-native-webview/lib/WebViewTypes';
 
 import { View } from '../components/Themed';
 
 export default function BruttoNettoScreen() {
+  const webviewRef = React.useRef<WebView>(null);
+
+  const handleWebViewNavigationStateChange = ({ url }: WebViewNativeEvent) => {
+    if (webviewRef.current == null || url == null) {
+      return;
+    }
+    if (!url.includes('f7-site')) {
+      webviewRef.current.stopLoading();
+      Linking.openURL(url).catch(console.error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://f7-site.vercel.app' }} />
+      <WebView
+        onNavigationStateChange={handleWebViewNavigationStateChange}
+        ref={webviewRef}
+        source={{ uri: 'https://f7-site.vercel.app' }}
+      />
     </View>
   );
 }
